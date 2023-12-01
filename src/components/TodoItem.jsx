478c4fc3974e155b308e77ebb4ed20a70a2f7d29
Anshuman Sathua <co.anshu@gmail.com>
2023-12-01 22:56:10 +0530

    bug fixed

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,9 +3,10 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import Layout from "./Layout";
 import { getTodoById, updateTodo, deleteTodo } from "../services/indexedDB";
 
-const TodoItem = ({ todo }) => {
+const TodoItem = () => {
   const { id } = useParams();
   const [currentTodo, setCurrentTodo] = useState(null);
   const [comment, setComment] = useState("");
@@ -31,13 +32,13 @@ const TodoItem = ({ todo }) => {
 
   const handleEdit = () => {
     // Redirect to the edit page with the todo item details
-    navigate(`/add/${todo.id}`, { state: { todo } });
+    navigate(`/add/${currentTodo.id}`, { state: { currentTodo } });
   };
 
   const handleDelete = async () => {
     // Implement your delete logic here
     try {
-      await deleteTodo(todo.id);
+      await deleteTodo(currentTodo.id);
       // Redirect to the home page after successful deletion
       navigate("/home");
     } catch (error) {
@@ -63,94 +64,96 @@ const TodoItem = ({ todo }) => {
   }
 
   return (
-    <div className="max-w-md mx-auto mt-4 p-4 bg-white rounded-md shadow-md">
-      <h2 className="text-xl font-bold mb-2">{todo.title}</h2>
-      <p className="text-gray-700 mb-2">{todo.description}</p>
-      <p className="text-gray-700 mb-2">
-        Due Date: {new Date(todo.dueDate).toLocaleDateString()}
-      </p>
-      <p className="text-gray-700 mb-2">Label: {todo.label}</p>
-      <p className="text-gray-700 mb-2">
-        Completed: {todo.isChecked ? "Yes" : "No"}
-      </p>
+    <Layout>
+      <div className="max-w-md mx-auto mt-4 p-4 bg-white rounded-md shadow-md">
+        <h2 className="text-xl font-bold mb-2">{currentTodo.title}</h2>
+        <p className="text-gray-700 mb-2">{currentTodo.description}</p>
+        <p className="text-gray-700 mb-2">
+          Due Date: {new Date(currentTodo.dueDate).toLocaleDateString()}
+        </p>
+        <p className="text-gray-700 mb-2">Label: {currentTodo.label}</p>
+        <p className="text-gray-700 mb-2">
+          Completed: {currentTodo.isChecked ? "Yes" : "No"}
+        </p>
 
-      {/* Comment section */}
-      <div className="mt-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2">
-          Add Comment
-        </label>
-        <div className="flex">
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            placeholder="Enter your comment"
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
-          />
+        {/* Comment section */}
+        <div className="mt-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Add Comment
+          </label>
+          <div className="flex">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              placeholder="Enter your comment"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+            <button
+              className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              onClick={handleAddComment}
+            >
+              Add
+            </button>
+          </div>
+        </div>
+
+        {/* Display comments */}
+        <div className="mt-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Comments
+          </label>
+          <ul>
+            {comments.map((commentObj, commentIndex) => (
+              <li key={commentIndex} className="text-gray-700">
+                {commentObj.text}
+                {commentObj.replies.length > 0 && (
+                  <ul className="ml-4">
+                    {commentObj.replies.map((replyText, replyIndex) => (
+                      <li key={replyIndex}>{replyText}</li>
+                    ))}
+                  </ul>
+                )}
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    placeholder="Reply to this comment"
+                    value={reply}
+                    onChange={(e) => setReply(e.target.value)}
+                  />
+                  <button
+                    className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="button"
+                    onClick={() => handleReply(commentIndex)}
+                  >
+                    Reply
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Edit and Delete buttons */}
+        <div className="flex mt-4 space-x-4">
           <button
-            className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
-            onClick={handleAddComment}
+            onClick={handleEdit}
           >
-            Add
+            Edit
+          </button>
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={handleDelete}
+          >
+            Delete
           </button>
         </div>
       </div>
-
-      {/* Display comments */}
-      <div className="mt-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2">
-          Comments
-        </label>
-        <ul>
-          {comments.map((commentObj, commentIndex) => (
-            <li key={commentIndex} className="text-gray-700">
-              {commentObj.text}
-              {commentObj.replies.length > 0 && (
-                <ul className="ml-4">
-                  {commentObj.replies.map((replyText, replyIndex) => (
-                    <li key={replyIndex}>{replyText}</li>
-                  ))}
-                </ul>
-              )}
-              <div className="mt-2">
-                <input
-                  type="text"
-                  placeholder="Reply to this comment"
-                  value={reply}
-                  onChange={(e) => setReply(e.target.value)}
-                />
-                <button
-                  className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="button"
-                  onClick={() => handleReply(commentIndex)}
-                >
-                  Reply
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      {/* Edit and Delete buttons */}
-      <div className="flex mt-4 space-x-4">
-        <button
-          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="button"
-          onClick={handleEdit}
-        >
-          Edit
-        </button>
-        <button
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="button"
-          onClick={handleDelete}
-        >
-          Delete
-        </button>
-      </div>
-    </div>
+    </Layout>
   );
 };
 
